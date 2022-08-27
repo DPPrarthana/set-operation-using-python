cf6f8bc76debcc96d918d78ceaf272256f6a6103
DPPrarthana <111065819+DPPrarthana@users.noreply.github.com>
2022-08-27 19:48:28 +0530

    Add files via upload

---
+++ set operation using python.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+E = {0,2,4,6,8 }
+N = {1,2,3,4,5}
+print("union :" , E | N)
+print("intersection :" , E & N)
+print("difference :" , E - N)
+print("symmetric difference :" , E ^ N)
+
+
+# In[ ]:
+
+
+
+
